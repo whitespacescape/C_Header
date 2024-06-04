eee1e1af85274666547c64d53eebf1e959dcff89
spacescape <15368104568@189.cn>
2024-06-04 23:31:44 +0800

    Update return.h

--- Header/return.h
+++ Header/return.h
@@ -1,8 +1,6 @@
-#ifdef returnn
 #include "../RETURN/returnn.c"
 #include "../RETURN/returnnn.c"
 #include "../RETURN/returnnnn.c"
 #include "../RETURN/returnnnnn.c"
 #include "../RETURN/returnnnnnn.c"
 #include "../RETURN/returnnnnnnn.c"
-#endif
